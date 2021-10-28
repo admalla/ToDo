d4f7b9f45770c8da9c9122bff0a7efd70fdb567d
admalla <maasovec@gmail.com>
2021-10-28 16:40:35 +0300

    push todo

---
+++ app.js
@@ -0,0 +1,52 @@
+let inp = document.getElementById('inp');
+let addBtn = document.getElementById('addBtn');
+let tasks = document.getElementById('tasks');
+let countNew = document.querySelector('._new');
+let countAccepted = document.querySelector('._accepted');
+
+tasksArray = [];
+
+displayTasks = () => {
+  tasks.innerHTML = '';
+  tasksArray.forEach((element, i) => {
+    if (element.text) {
+      tasks.innerHTML += `
+  <div class="box ${element.completed && 'completed'}">
+          <span class="text">${element.text}</span>
+          <div>
+            <input id='${i}' type="checkbox" ${element.completed && 'checked'}/>
+            <i id='${i}' class="fa fa-times" aria-hidden="true"></i>
+          </div>
+        </div>
+  `;
+    } else return '';
+  });
+  count();
+};
+
+addBtn.addEventListener('click', () => {
+  tasksArray.push({ text: inp.value, completed: false });
+  inp.value = '';
+  displayTasks();
+});
+
+tasks.addEventListener('click', (e) => {
+  if (e.target.type === 'checkbox') {
+    tasksArray[e.target.id].completed = !tasksArray[e.target.id].completed;
+  } else if (e.target.className === 'fa fa-times') {
+    tasksArray.splice(e.target.id, 1);
+  }
+  displayTasks();
+});
+
+let count = () => {
+  countAccepted.innerHTML = 0;
+  countNew.innerHTML = 0;
+  tasksArray.forEach((e) => {
+    if (e.completed) {
+      countAccepted.innerHTML++;
+    } else {
+      countNew.innerHTML++;
+    }
+  });
+};
